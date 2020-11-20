a1e3e927dea2d66da97b6adafdf2d588f3a17156
dengyuning <13552940428@163.com>
2020-11-20 16:06:30 +0800

    Create 访问带空格的文件夹.sh

---
+++ utils/linux/scripts/访问带空格的文件夹.sh
@@ -0,0 +1,7 @@
+wavdir="/Users/vera/Documents/Jin/OneDrive - McGill University/McGill/Projects/Eval 1/phrasing_focus/64mp3-v1"
+echo $wavdir
+cd "$wavdir"
+
+for i in "$wavdir"/*; do
+ echo $i
+done
